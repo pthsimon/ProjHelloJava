5da1f9dc01819e4dfb556651a3a60c469d166cf8
pthsimon <simca212@gmail.com>
2017-02-10 17:20:10 +0800

    another try
    blah

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello world!");
+        System.out.println("Hello world!");
+       ///just a check
 		System.out.println("another edit from github editor");
+		System.out.println("Hello world!2");
+        //another check
+		System.out.println("Hello world!3");
 	}
 
 }
